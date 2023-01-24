2657e06117985ddf93202d8610474abfaec9ce31
alex <alexmuthini3@gmail.com>
2023-01-24 17:48:31 +0300

    corrected requirement fail v2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,31 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order
- * using selection sort algorithm.
- *
- * @array: array to sort
+ * selection_sort - sorts an array of integers in ascending order using
+ * the 'Selection sort' algorithm
+ * @array: the array to sort
  * @size: size of the array
- * Return: void
+ * Retrun: void
  */
-
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
+	size_t i, j, min, tmp;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
 	{
 		min = i;
-		for (j = i + 1; j < size; j++)
-		{
+		for (j = i; j < size; j++)
 			if (array[j] < array[min])
+			{
 				min = j;
-		}
-
+			}
 		if (min != i)
 		{
-			swap(&array[i], &array[min]);
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
 			print_array(array, size);
 		}
 	}
